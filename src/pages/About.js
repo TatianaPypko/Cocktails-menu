a61e548a36a47fbd1c8de74fd43a848eb9f316c5
TatianaPypko <tatiana.pypko@gmail.com>
2023-06-21 13:24:19 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h2 className="section-title">about us</h2>
+      <p>
+        Because as much as you love good design and inspiring illustrations, you
+        also want to meet the people that curate all the content for you.
+        Furthermore, it’s equally rewarding when you realize that they are just
+        as eager to start a visual dialogue with you. Nathan Strandberg and
+        Katie Kirk are doing what makes them happy, and this is obvious
+        throughout their page.
+      </p>
+    </section>
+  );
+};
+
+export default About;
